a90ac099236fddb931cc2dc96bd0879ace30ea61
ValtierraXavier <xavier.valtierra@icloud.com>
2023-10-19 00:50:10 -0400

    countdown Successful fix clearinterval bug next

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -2,14 +2,14 @@ import React,{useState} from "react";
 import './Timer.css'
 
 export default function Timer () {
-    let sec = 0;
+    let sec, secs;
     let min = 0;
     const[adjustTime ,setAdjustTime] = useState(true)
     const[duration, setDuration] = useState(0)
     const[timerInterval, setTimerInterval] = useState(null)
-    const[seconds, setSeconds] = useState(null)
+    const[seconds, setSeconds] = useState(0)
+    const[minutes, setMinutes] = useState(0)
     const doneArr =['Timer', 'Done!!']
-    // const[minutes, setMinutes] = useState()
 
     const doneTiming =(ms) => {
         return new Promise(
@@ -18,45 +18,99 @@ export default function Timer () {
     }
 
     const startTimer = ()=>{
+        resetTimer();
         setAdjustTime(prev => prev = false) 
-        sec = duration * 60
+        const totalSeconds = duration * 60;
+        // const diff = 0
+        sec = 0
+        min = duration
+        setMinutes(prev => prev = min)
+        setSeconds(prev => prev = sec)
         setTimerInterval(setInterval(countDown, 1000))
     } 
 
-    const doneTitle = async () => {
-        for(let i = 0 ; i < doneArr.length; i++){ 
-            try{            
-                await doneTiming(500);
-                if(i === doneArr.length){
-                    i = 0
-                }
-                document.title = doneArr[i]
-            }catch(error){console.log(error.message)}
-        }
+    const done = () => {  
+        clearInterval(timerInterval)
+        setAdjustTime(prev => prev = true)
     }
 
-    const done = async () => {  
-        clearInterval(timerInterval)
-        doneTitle()
-    }
     const countDown = () => {
-        if(sec <= 0){
-           done();
-        }
-        sec--
-        setSeconds(prev => prev = sec < 0 ? 0 : sec)
+        if(sec <= 0 && min <= 0){
+            resetTimer();
+            return
+        }else if( sec <= 0 && min > 0 ){
+            min--
+            setMinutes(prev => prev = min)
+            if(sec == 0){
+                sec = 60
+                setSeconds(prev => prev = sec)
+            }
+            if(sec == 0 && min == 0){
+                resetTimer()
+                return
+            }
+        } 
+        if(sec > 0 && min >= 0){
+            if(sec == 0){
+                sec = 59
+                setSeconds(prev => prev = sec)
+                min--
+                setMinutes(prev => prev = min)
+            }
+            sec--
+            setSeconds(prev => prev = sec)
+            if(sec == 0 && min == 0){
+                resetTimer()
+                return
+            }
+        } 
+        if(sec > 0 && min < 1 && min > 0){
+            min = 0 ;
+            setMinutes(prev => prev = min)
+            sec--
+            setSeconds(prev => prev = sec)
+            if(sec == 0 && min == 0){
+                resetTimer()
+                return
+            }
+           
+        } 
+        if(sec == 0 && min == 0){
+            resetTimer()
+            return
+        }       
     }
 
     const resetTimer = () => {
-         
+        console.log('clear')
+        clearInterval(timerInterval);
+        sec = 0;
+        min = 0;
+        setSeconds( prev => prev = sec);
+        setDuration(prev => prev = min);
+        setAdjustTime(prev => prev = true);
+        document.title = 'Simple Timer';
     }
 
     return(
         <div className="Timer">
+            {/* <div className='timerLabel'>minutes</div> */}
             {adjustTime?
-                <input type = "number" className = 'countDown'onChange={(e)=>setDuration(prev => prev =e.target.value)} value = {duration}></input>
+                <div className ='timeDiv' id='adjustable' >
+                    <label className = "timeLabel" id = "minuteslabel">minutes</label>
+                    <input type = "number" className = 'countDown'onChange={(e)=>setDuration(prev => prev =e.target.value)} value = {duration}></input>
+                </div>
                 :
-                <div className = 'countDown'>{seconds}</div>
+                <div className ='timeDiv' id='nonAdjustable'>
+                    <div className = 'minutesDiv'>
+                        <label className = 'timeLabel' id = 'minuteslabel' >minutes</label>
+                        <div className = 'minutesCountDown'>{minutes}</div>
+                    </div>
+                    <div className = 'secondsDiv'>
+                        <label className = 'timeLabel' id = 'secondslabel' >seconds</label>
+                        <div className = 'secondsCountDown'>{seconds}</div>
+                    </div>
+                </div>
             }
             
             <section className="actionButtons">
